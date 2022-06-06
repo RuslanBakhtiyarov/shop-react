57be5c605a7a02d3079556a85bd849e56eb83448
RuslanBakhtiyarov <bakhtiyarovruslan1@gmail.com>
2022-06-06 14:39:26 +0300

    ok1

---
+++ src/components/Categories.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+export class Categories extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      categories: [
+        {
+          key: 'all',
+          name:'Все'
+        },
+        {
+          key: 'chairs',
+          name:'Стулья'
+        },
+        {
+          key: 'tabless',
+          name:'Столы'
+        },
+        {
+          key: 'light',
+          name:'Светильники'
+        },
+        {
+          key: 'sofa',
+          name:'Диваны'
+        },
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className='categories'>
+        {this.state.categories.map(el => (
+          <div key={el.key}
+            onClick={() => this.props.chooseCategory(el.key)}
+          >
+            {el.name}</div>
+))}
+      </div>
+    )
+  }
+}
+
+export default Categories
